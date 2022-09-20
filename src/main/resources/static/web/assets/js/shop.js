cf7fb78e14cc8aec36f655d3d6d8d8e464e5fd58
alan <alan-joelm@hotmail.com>
2022-09-20 18:09:30 -0300

    agregada la funcion para comprar con la app del homebanking (de aleTorres)

--- src/main/resources/static/web/assets/js/shop.js
+++ src/main/resources/static/web/assets/js/shop.js
@@ -4,103 +4,116 @@ createApp({
     data() {
         return {
             products: [],
-            filteredProducts : [],
+            filteredProducts: [],
             productsBg: ["../web/assets/images/produc-bg-01.png", "../web/assets/images/produc-bg-02-01.png"],
             categories: ["Suplements", "Equipment", "Clothes"],
             suplements: ["Proteins", "Creatines", "BCAA"],
             clothes: ["Men", "Women"],
-            checkedCategory : "",
+            checkedCategory: "",
             subcategories: [],
-            inputRange : 0,
-            searchFilterInput : "",
-            isOpen : false,
-            cartProducts : [],
-            moneyFormat : new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }),
-            featureds : [],
-            noStock : false,
-            addCartAllert : false,
+            inputRange: 0,
+            searchFilterInput: "",
+            isOpen: false,
+            cartProducts: [],
+            moneyFormat: new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }),
+            featureds: [],
+            noStock: false,
+            addCartAllert: false,
+            subtotal : 0
         }
     },
     created() {
         this.loadData()
         const localStorageData = JSON.parse(localStorage.getItem("productsInCart"))
-        if(localStorageData == null){
+        if (localStorageData == null) {
             this.cartProducts = []
-        }else{
+        } else {
             this.cartProducts = localStorageData
         }
     },
     methods: {
-        deleteItemCart(product){
-            if(product.quantity > 1){
-                product.quantity --
-                product.stock ++
+        makePayment() {
+            axios.post('https://hubbersbank.herokuapp.com/api/transactions/payment', {
+                "cardNumber": "5223 2602 9373 6997",
+                "cardCvv": "125",
+                "amount": "1",
+                "thruDate": "2027-09-20",
+                "cardHolder": "Melba Laflor",
+                "accountNumber": "VIN001",
+                "description": "pago balcamgym"
+            }).then(r => console.log(r))
+        },
+        deleteItemCart(product) {
+            if (product.quantity > 1) {
+                product.quantity--
+                product.stock++
                 localStorage.setItem("productsInCart", JSON.stringify(this.cartProducts))
-            }else{
-                product.stock ++
+            } else {
+                product.stock++
                 this.cartProducts.splice(this.cartProducts.indexOf(product), 1)
                 localStorage.setItem("productsInCart", JSON.stringify(this.cartProducts))
             }
         },
-        cleanCart(){
+        cleanCart() {
             this.cartProducts = []
             localStorage.setItem("productsInCart", JSON.stringify(this.cartProducts))
         },
-        noStockAllert(){
+        noStockAllert() {
             this.addCartAllert = false
             this.noStock = true
             setTimeout(() => {
                 this.noStock = false
             }, 5000)
         },
-        addCart(product){
+        addCart(product) {
             this.addCartAllert = true
             this.noStock = false
-            setTimeout(()=>{
+            this.subtotal = product.price
+            setTimeout(() => {
                 this.addCartAllert = false
             }, 3000)
-            if(this.cartProducts.includes(product)){
-                product.quantity ++
+            if (this.cartProducts.includes(product)) {
+                product.quantity++
                 product.stock--
                 localStorage.setItem("productsInCart", JSON.stringify(this.cartProducts))
-                if(product.stock < 0 ){
+                if (product.stock < 0) {
                     this.noStockAllert()
                 }
-            }else{
+            } else {
                 product.quantity = 1
-                product.stock --
+                product.stock--
                 this.cartProducts.push(product)
                 localStorage.setItem("productsInCart", JSON.stringify(this.cartProducts))
             }
         },
         loadData() {
             axios.get("/api/products")
-            .then(response => {
-                this.products = response.data
-                console.log(this.products)
-                this.filteredProducts = this.products
-            }).catch(error => error)
+                .then(response => {
+                    this.products = response.data
+                    console.log(this.products)
+                    this.filteredProducts = this.products
+                }).catch(error => error)
         },
-        selectFilter(category){
+        selectFilter(category) {
             this.checkedCategory = category
-            if(category == "Suplements"){
+            if (category == "Suplements") {
                 this.subcategories = this.suplements
                 this.filteredProducts = this.products.filter(product => product.productCategory == "SUPPLEMENTS")
-            }else if(category == "Clothes"){
+            } else if (category == "Clothes") {
                 this.subcategories = this.clothes
                 this.filteredProducts = this.products.filter(product => product.productCategory == "CLOTHES")
-            }else if(category == "Equipment"){
+            } else if (category == "Equipment") {
                 this.subcategories = []
                 this.filteredProducts = this.products.filter(product => product.productCategory == "EQUIPMENT")
             }
         },
-        rangePriceFilter(inputRange){
+        rangePriceFilter(inputRange) {
             this.filteredProducts = this.products.filter(product => product.price <= inputRange)
         },
-        searchFilter(){
+        searchFilter() {
             this.filteredProducts = this.products.filter(product => product.name.toLowerCase().includes(this.searchFilterInput))
             console.log(this.filteredProducts)
-            if(this.searchFilterInput == ""){
+            if (this.searchFilterInput == "") {
                 this.filteredProducts = this.products
             }
         }
